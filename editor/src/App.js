aca9063ac858fa2961e1e230cf43ab17dd0ec555
Nathan Kleekamp <nkleekamp@gmail.com>
2019-09-04 22:31:35 -0400

    remove unnecessary styles

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { MainMenu } from './components';
-
 import './App.css';
 
 function App() {
   return (
     <div className="o-App">
       <MainMenu />
-      <div className="o-ContentWrapper">
-        <div className="c-Sidebar"></div>
-        <div className="c-Content"></div>
-      </div>
+      <div className="c-Content"></div>
     </div>
   );
 }
